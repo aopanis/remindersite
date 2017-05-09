980b23229a90369dcb18f43154d19a6de063159c
Anton Panis <aopanis@gmail.com>
2017-05-09 16:55:37 -0700

    Comments for reminder viewing and adding functionality

--- reminder_list/views.py
+++ reminder_list/views.py
@@ -5,15 +5,18 @@ from django.shortcuts import render
 from .models import Reminder
 
 
+# View for the index of the website, displays the index template
 def index(request):
     reminders = Reminder.objects.all()
     return render(request, 'reminder_list/index.html', {'reminders': reminders})
 
 
+# View to add a new reminder, displays the add_reminder template
 def add_reminder(request):
     return render(request, 'reminder_list/add_reminder.html', {})
 
 
+# View for creating a new reminder, receives info from add_reminder template and adds a new entry to the DB
 def successful_add(request):
     new_reminder = Reminder(title=request.POST['title'],
                             reminder_time=dateutil.parser.parse(request.POST['reminder_time']))
