42819986feb07dadfd81c8ca4fd30a73f8bb6c58
Anton Panis <aopanis@gmail.com>
2017-05-09 16:47:32 -0700

    Added support for displaying and adding reminders

--- reminder_list/urls.py
+++ reminder_list/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'reminder_list'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^add$', views.add_reminder, name='add reminder'),
+    url(r'^successful_add$', views.successful_add, name='successful_add'),
 ]
--- reminder_list/views.py
+++ reminder_list/views.py
@@ -1,7 +1,21 @@
-from django.http import HttpResponse
+import dateutil.parser
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from .models import Reminder
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the test page for the reminder list.")
+    reminders = Reminder.objects.all()
+    return render(request, 'reminder_list/index.html', {'reminders': reminders})
 
+
+def add_reminder(request):
+    return render(request, 'reminder_list/add_reminder.html', {})
+
+
+def successful_add(request):
+    new_reminder = Reminder(title=request.POST['title'],
+                            reminder_time=dateutil.parser.parse(request.POST['reminder_time']))
+    new_reminder.save()
+    return HttpResponseRedirect(reverse('reminder_list:index'))
